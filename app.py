697a7259d7bde79600f5d0be0d0c7fe577d5fb45
Iara Souza <iaradsouza1@gmail.com>
2020-07-05 19:09:19 -0300

    "Added plots.py. New app.py"

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import os
 import streamlit as st
 from datetime import date
 import compute_metrics as cm
+import pandas as pd
+import plots
+from datetime import date
 
 states_names = [
     'AC', 'AL', 'AM', 'AP',
@@ -13,17 +16,27 @@ states_names = [
     'SE', 'SP', 'TO'
 ]
 
+RESULTS = 'results.pkl'
+GEOJSON = 'data/brasil_estados.geojson'
+
 def main():
 
-    today = date.today()
-    file_current = 'data/Covid-19-Brasil_' + str(today) + '.csv'
+    # TO DO: fix download
+    # today = date.today()
+    # file_current = 'data/Covid-19-Brasil_' + str(today) + '.csv'
 
-    # TO DO: CREATE A SCRIPT TO CALL compute_metrics.py
-    if not os.path.isfile(file_current):
-        cm.main()
+    # # TO DO: CREATE A SCRIPT TO CALL compute_metrics.py
+    # if not os.path.isfile(file_current):
+    #     cm.main()
 
     st.title('COVID-19 directionality app')
+
+    df_results = plots.pickle_to_df(RESULTS, GEOJSON)
+    last_day = st.date_input('Date') 
     page = st.sidebar.radio('Select level:', ('Nation-wide statistics', 'By state statistics'), index=0)
+
+    # Show map
+    st.plotly_chart(plots.plot_map(df_results, last_day, GEOJSON))
 
     if page == 'Nation-wide statistics':
         st.header('Best Rt estimation for all states')
@@ -31,22 +44,19 @@ def main():
         st.header('Infection spreading by states, according to their level of social distancing adopted by local governments:')
         st.image('plot/High.png', use_column_width=True)
         st.header('States currently not undercontrol:')
-        st.image('plot/not_undercontrol.png', use_column_width=True)
+        st.image('plot/not_undercontrol.png')
         st.header('States currently undercontrol:')
-        st.image('plot/undercontrol.png', use_column_width=True)
+        st.image('plot/undercontrol.png')
 
     elif page == 'By state statistics':
         state = st.sidebar.selectbox('Select a state:', states_names)
-        new_cases = 'plot/' + str(state) + '_new_cases_per_day.png'
-        posteriors = 'plot/' + str(state) + '_posteriors.png'
-        hdis = 'plot/' + str(state) + '_hdis.png'
 
-        st.header(f'How many cases of COVID-19 {state} has in a daily basis?')
-        st.image(new_cases, use_column_width=True)
-        st.header('Estimate Rt likelihood')
-        st.image(posteriors, use_column_width=True)
-        st.header('Best Rt estimation by day')
-        st.image(hdis, use_column_width=True)
+        st.header('Check new cases by day')
+        st.plotly_chart(plots.plot_new_cases(state))
+
+        st.header('Check Rt by day')    
+        st.plotly_chart(plots.plot_rt_state(state, df_results))
+
 
 if __name__ == "__main__":
     main()
---
+++ plots.py
@@ -0,0 +1,177 @@
+import pickle
+import matplotlib.pyplot as plt
+import plotly.graph_objs as go
+import plotly.express as px
+import plotly.figure_factory as ff
+import pandas as pd
+import numpy as np
+import json
+import streamlit as st
+
+from datetime import date
+
+@st.cache
+def pickle_to_df(results_pkl, geojson):
+    
+    # Read 'results.pkl' file
+    with open(results_pkl, 'rb') as posteriors, open(geojson) as geoj:
+        df_hdi = pickle.load(posteriors)
+        brasil = json.load(geoj)
+
+        # Get geocodes
+        df_geocode = dict()
+        for i in brasil['features']:
+            estado = i['properties']['uf_05']
+            geocodigo = i['properties']['geocodigo']
+            df_geocode[estado] = geocodigo
+    
+        # Create the columns 'state' and 'geocodes'
+        for k in df_hdi.keys():
+            df_hdi[k]['state'] = k
+            df_hdi[k]['geocode'] = df_geocode[k]
+            df_hdi[k] = df_hdi[k].reset_index()
+        
+        # Concatenate all dfs
+        df_results = pd.concat([v for k, v in df_hdi.items()])
+
+        return(df_results)
+
+def plot_map(df_results, date, geojson):
+
+    with open(geojson) as geoj:
+        brasil = json.load(geoj)
+
+    date = pd.to_datetime(date)
+
+    df_tmp = df_results[df_results.date.eq(date)]
+
+    fig = go.Figure(
+        go.Choroplethmapbox(
+            geojson=brasil, 
+            locations=df_tmp.geocode, 
+            text=df_tmp.state,
+            z=df_tmp.ML,
+            colorscale=px.colors.sequential.OrRd, 
+            zmin=0, 
+            zmax=4,
+            featureidkey='properties.geocodigo',
+            hoverinfo='text+z'
+        )
+    )
+
+    fig.update_layout(
+        mapbox_style="carto-positron", 
+        mapbox_zoom=3, 
+        mapbox_center={"lat": -15.5517, "lon": -58.7073},
+        margin={"r":0,"t":0,"l":0,"b":0}
+    )
+    
+    return(fig)
+
+def plot_new_cases(state):
+
+    original_path =  'metrics/' + str(state) + '_original.pkl'
+    smoothed_path = 'metrics/' + str(state) + '_smoothed.pkl'
+    with open(original_path, 'rb') as original, open(smoothed_path, 'rb') as smoothed:
+        df_original = pickle.load(original)
+        df_smoothed = pickle.load(smoothed)
+
+    df = pd.DataFrame({
+        'date': df_original.xs(state, level='state').index,
+        'cases': df_original.xs(state, level='state').values,
+        'smoothed': df_smoothed.xs(state, level='state').values
+    })
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=df['date'],
+        y=df['cases'],
+        name='Nº de casos',
+        line=dict(
+            color='rgb(90,128,216)'
+        )
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=df['date'],
+        y=df['smoothed'],
+        name='Smoothed',
+        line=dict(
+            dash='dot',
+            color='rgb(215,80,70)'
+        ),
+        hoverinfo='text+z'
+    ))
+
+    fig.update_layout(
+        plot_bgcolor='white',
+        xaxis_title='Dias',
+        yaxis_title='Novos casos por dia',
+    )
+
+    fig.update_xaxes(showgrid=True, gridcolor='rgb(215,215,215)')
+    fig.update_yaxes(showgrid=True, gridcolor='rgb(215,215,215)')
+
+    return(fig)
+
+
+def plot_rt_state(state, df_results):
+
+    df_tmp = df_results[ df_results.state.eq(state) ]
+
+    upper = go.Scatter(
+        name = 'Upper bound',
+        x = df_tmp.date,
+        y = df_tmp.High,
+        mode = 'lines',
+        marker=dict(color="#444"),
+        line=dict(width=0),
+        connectgaps=True
+    )
+
+    rt = go.Scatter(
+        name = 'Rt',
+        x = df_tmp.date,
+        y = df_tmp.ML,
+        mode = 'lines',
+        line = dict(
+            color = 'rgba(225,65,65,255)'
+        ),
+        connectgaps=True,
+        fillcolor='rgba(154, 143, 200, 0.3)',
+        fill='tonexty'
+    )
+
+    low = go.Scatter(
+        name = 'Lower Bound',
+        x = df_tmp.date,
+        y = df_tmp.Low,
+        marker = dict(color="#444"),
+        line = dict(width = 0),
+        mode='lines',
+        connectgaps=True,
+        fillcolor='rgba(154, 143, 200, 0.3)',
+        fill='tonexty'
+    )
+
+    data = [upper, rt, low]
+
+    layout = go.Layout(
+        showlegend = False,
+        plot_bgcolor='white',
+        xaxis=dict(
+            gridcolor='rgb(215,215,215)'
+        ),
+        yaxis = dict(
+            title='Rt',
+            gridcolor='rgb(215,215,215)'
+        ),
+        hovermode='x'
+    )
+
+    fig = go.Figure(data = data, layout = layout)
+
+    return(fig)
+
+#def show_bars():
